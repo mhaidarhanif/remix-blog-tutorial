0c5e879f1d476161842de24db786f5d3c7a193eb
M Haidar Hanif <git@mhaidarhanif.com>
2021-12-27 19:58:09 +0700

    chore: setup for vercel

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,10 @@
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildDirectory: "build",
+  appDirectory: 'app',
+  assetsBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  serverBuildDirectory: 'api/_build',
   devServerPort: 8002,
-  ignoredRouteFiles: [".*"]
-};
+  ignoredRouteFiles: ['.*'],
+}
